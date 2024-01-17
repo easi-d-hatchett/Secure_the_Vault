be234d8311e4292ccd59bb7ed021e18a6bcf252f
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-01-17 16:28:20 -0500

    finished excer

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let num1 = 5 + 5;
+
+let num2 = 80 / 2;
+
+let num3 = 40 - 1;
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+alert(message)
+alert(num1 + "-" + num2 + "-" + num3); 
